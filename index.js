53bcf9ec594042b8e01bbb4b6ab41c57c5c6ff07
devgenx <sebastian110420@gmail.com>
2022-12-14 13:17:22 -0500

    commit tree 61d99cb4741ee78d6c65d5f8fae944917a623526

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// Code your solutions in this file
+
+let arrays = ["Charlie", "Samip", "Ali"];
+
+function writeCards(array, events) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    newArr.push(`Thank you, ${array[i]}, for the wonderful ${events} gift!`);
+  }
+  return newArr;
+}
+
+writeCards(arrays, "birthday");
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num--);
+  }
+}
+
+countDown(4);
